2bd876f65c8f1fffe535131dbe3e45601d526793
Grant Wuerker <gwuerker@gmail.com>
2019-10-16 21:00:49 +0900

    compiles

--- contracts/SmartRecovery.sol
+++ contracts/SmartRecovery.sol
@@ -1,11 +1,11 @@
-pragma solidity >=0.4.21 <0.6.0;
+pragma solidity >=0.5.0 <0.6.0;
 
 contract SmartRecovery {
   struct Account {
     uint8[] questions;
     uint256 balance;
     bytes32 target;
-    bytes32 salt;
+    uint256 salt;
   }
 
   struct Proof {
@@ -22,8 +22,9 @@ contract SmartRecovery {
 
   function createAccount(
     bytes32 key,
-    uint8[] questions,
-    bytes32 target
+    uint8[] memory questions,
+    bytes32 target,
+    uint256 salt
   )
     public
     payable
@@ -31,8 +32,9 @@ contract SmartRecovery {
     accounts[key] = Account(
       questions,
       msg.value,
-      target
-    )
+      target,
+      salt
+    );
   }
 
   function startRecovery(
@@ -43,7 +45,7 @@ contract SmartRecovery {
   {
     proofs[msg.sender].key = key;
     proofs[msg.sender].commit = commit;
-    proofs[msg.sender].reveal = reveal;
+    proofs[msg.sender].time = now;
   }
 
   function finishRecovery(
@@ -54,10 +56,10 @@ contract SmartRecovery {
   {
     proofs[msg.sender].reveal = reveal;
     proofs[msg.sender].salt = salt;
-    if validProof(msg.sender) {
+    if (validProof(msg.sender)) {
       msg.sender.transfer(
         accounts[proofs[msg.sender].key].balance
-      )
+      );
     }
   }
 
@@ -68,12 +70,12 @@ contract SmartRecovery {
     view
     returns (bool)
   {
-    Proof proof = proofs[proover];
-    Account account = accounts[proof.key];
+    Proof memory proof = proofs[proover];
+    Account memory account = accounts[proof.key];
     return now - proof.time > 2 minutes &&
            sha256(
              abi.encodePacked(proof.reveal, '-', proof.salt)
-           ) == commit &&
-           sha256(reveal) == account.target;
+           ) == proof.commit &&
+           sha256(abi.encode(proof.reveal)) == account.target;
   }
 }
